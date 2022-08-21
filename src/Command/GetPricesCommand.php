b936ed2c86ffa6e671a4e23b4db1a52d1cd13ffc
Pawel <krotowski.p@gmail.com>
2022-08-21 21:32:50 +0200

    command + service

--- src/Command/GetPricesCommand.php
+++ src/Command/GetPricesCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Command;
 
+use App\Service\PriceImporter;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -12,14 +13,25 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 
 #[AsCommand(
     name: 'app:get:prices',
-    description: 'Add a short description for your command',
+    description: 'Get ammo prices from dedicated gunstore',
 )]
 class GetPricesCommand extends Command
 {
+    private  $priceImporter;
+
+    public function __construct(PriceImporter $priceImporter)
+    {
+        $this->priceImporter = $priceImporter;
+
+        parent::__construct();
+    }
+
+
     protected function configure(): void
     {
         $this
-            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
+            ->addArgument('caliber', InputArgument::OPTIONAL, 'Caliber')
+            ->addArgument('store', InputArgument::OPTIONAL, 'store')
             ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
         ;
     }
@@ -27,17 +39,24 @@ class GetPricesCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
+        $caliber = $input->getArgument('caliber');
+        $store = $input->getArgument('store');
 
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
+        $price = $this->priceImporter->getPrice();
+
+
+        if ($caliber) {
+            $io->note(sprintf('You passed an argument: %s', $caliber));
+        }
+        if ($store) {
+            $io->note(sprintf('You passed an argument: %s', $store));
         }
 
         if ($input->getOption('option1')) {
             // ...
         }
 
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+        $io->success(sprintf('Price = %s', $price));
 
         return Command::SUCCESS;
     }
---
+++ src/Service/PriceImporter.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Service;
+
+class PriceImporter
+{
+
+    public function getPrice() : float{
+        return $price = 1.2;
+    }
+
+}
